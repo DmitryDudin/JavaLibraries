f05701b128bba30f95003a527d13d33b2ea1b6dc
Dmitry Dudin <dudin_dmitry@mail.ru>
2017-07-11 00:46:46 +0300

    fixed

---
+++ OpenCSV/src/main/java/CsvReaderClass.java
@@ -0,0 +1,39 @@
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import org.apache.commons.lang3.time.DateUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.List;
+
+public class CsvReaderClass {
+    public static List<StudentProfile> mapCsvStringToEnitty(String csvString) throws ParseException {
+        List<StudentProfile> studentProfiles = null;
+        try (com.opencsv.CSVReader reader = new com.opencsv.CSVReader(new BufferedReader(new StringReader(csvString), ','))) {
+            ColumnPositionMappingStrategy<StudentProfile> beanStrategy = new ColumnPositionMappingStrategy<>();
+            beanStrategy.setType(StudentProfile.class);
+            beanStrategy.setColumnMapping(new String[]{"name", "grade", "birthdayString", "email", "phone", "address"});
+            CsvToBean<StudentProfile> csvToBean = new CsvToBean<>();
+            studentProfiles = csvToBean.parse(beanStrategy, reader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (StudentProfile studentProfile : studentProfiles) {
+            Date birthDay = DateUtils.parseDate(studentProfile.getBirthdayString(), new String[]{"dd-MM-yyyy", "dd/MM/yyyy"});
+//            Date birthDay = DateUtils.parseDate(studentProfile.getBirthdayString(), "dd-MM-yyyy", "dd/MM/yyyy");
+            birthDay = DateTimeUtil.getDateForTimeZone("UTC", birthDay);
+            studentProfile.setBirthday(birthDay);
+        }
+        return studentProfiles;
+    }
+
+    /*public static void main(String[] args) throws ParseException {
+        String csvString = "Mike, 5, 01-01-2000, mike@gmail.com, 112233, NewYork\n"+
+                "John, 10, 09-09-2009, john@gmail.com, 22222, Mayami\n";
+        CsvReaderClass.mapCsvStringToEnitty(csvString);
+    }*/
+}
--- OpenCSV/src/test/java/TestClass.java
+++ OpenCSV/src/test/java/TestClass.java
@@ -1,53 +1,22 @@
-import com.opencsv.CSVReader;
-import com.opencsv.bean.ColumnPositionMappingStrategy;
-import com.opencsv.bean.CsvToBean;
-import org.apache.commons.lang3.time.DateUtils;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.StringReader;
 import java.text.ParseException;
-import java.util.Date;
-import java.util.List;
 
 public class TestClass {
 
     private static String csvString;
 
-    /*@Before
-    private static void prepareStr() {
-        csvString = "Mike, 5, 01-01-2000, mike@gmail.com, 112233, NewYork";
-    }*/
-
-    @Test
-    public void testWithSringReadercd() throws ParseException {
-        System.out.println();
-        List<StudentProfile> studentProfiles = null;
-        try (CSVReader reader = new CSVReader(new BufferedReader(new StringReader(csvString), ','))) {
-            ColumnPositionMappingStrategy<StudentProfile> beanStrategy = new ColumnPositionMappingStrategy<>();
-            beanStrategy.setType(StudentProfile.class);
-            beanStrategy.setColumnMapping(new String[]{"name", "grade", "birthdayString", "email", "phone", "address"});
-            CsvToBean<StudentProfile> csvToBean = new CsvToBean<>();
-            studentProfiles = csvToBean.parse(beanStrategy, reader);
-        } catch (IOException e) {
-
-        }
-
-        for (StudentProfile studentProfile : studentProfiles) {
-            Date birthDay = DateUtils.parseDate(studentProfile.getBirthdayString(), new String[]{"dd-MM-yyyy", "dd/MM/yyyy"});
-//            birthDay = DateTimeUtil.getDateForTimeZone("UTC", birthDay);
-            studentProfile.setBirthday(birthDay);
-        }
-//        return studentProfiles;
+    @Before
+    public void prepareStr() {
+        csvString = "Mike, 5, 01-01-2000, mike@gmail.com, 112233, NewYork\n" +
+                "John, 10, 09-09-2009, john@gmail.com, 22222, Chicago\n";
     }
 
-    @Test
-    public void testss(){
-        System.out.println();
 
+    @Test
+    public void testss() throws ParseException {
+        CsvReaderClass.mapCsvStringToEnitty(csvString);
     }
 }
 
