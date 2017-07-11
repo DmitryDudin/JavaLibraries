8a19c23edaf04328d57246ca35ec7e6d66ad39fc
dmitrid <dudin.dmitry83@gmail.com>
2017-07-11 16:05:42 +0100

    update json

--- JSONLibraries/src/main/java/JacksonProj.java
+++ JSONLibraries/src/main/java/JacksonProj.java
@@ -1,3 +1,7 @@
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONException;
 
@@ -13,6 +17,24 @@ public class JacksonProj {
         String englishLanguage = "en";
         ObjectMapper mapper = new ObjectMapper();
 
+        //update chinese values
+        if (isValuesContainsChineseLang(values)) {
+            HashMap<String, List<String>> map = mapper.readValue(values, new TypeReference<HashMap<String, List<String>>>() {
+            });
+            map.put(chineseLanguage, new ArrayList<>());
+            for (String enValue : map.get(englishLanguage)) {
+                if (enValue == null) {
+                    map.get(chineseLanguage).add(null);
+                    continue;
+                }
+//            translate
+                map.get(chineseLanguage).add("translate2");
+            }
+            String json = mapper.writeValueAsString(map);
+            return json;
+        }
+
+        //create chinese values
         String[] asArray = mapper.readValue(values, String[].class);
         HashMap<String, List<String>> resultMap = new HashMap<>();
         resultMap.put(englishLanguage, new ArrayList<>());
@@ -20,17 +42,31 @@ public class JacksonProj {
         for (String str : asArray) {
             resultMap.get(englishLanguage).add(str);
             if (str == null) {
-                resultMap.get(chineseLanguage).add(aNull);
+                resultMap.get(chineseLanguage).add(null);
                 continue;
             }
 //            translate
-            resultMap.get(chineseLanguage).add("translate");
+            resultMap.get(chineseLanguage).add("translate1");
         }
         String json = mapper.writeValueAsString(resultMap);
         return json;
     }
 
-    public boolean isTemplateContainsChineseLang(String template) {
-        return template.contains("label_zh");
+    public boolean isTemplateContainsChineseLang(String template) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(template);
+        boolean res = (root.findValue("label_zh") == null) ? false : true;
+        return res;
+//        return template.contains("\"label_zh\"");
+    }
+
+    public boolean isValuesContainsChineseLang(String values) throws IOException {
+//        JsonFactory jfactory = new JsonFactory();
+//        JsonParser jParser = jfactory.createJsonParser(values);
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(values);
+        return root.has("zh");
+//        return values.contains("\"zh\"");
     }
 }
--- JSONLibraries/src/test/java/JacksonProjTest.java
+++ JSONLibraries/src/test/java/JacksonProjTest.java
@@ -6,14 +6,18 @@ public class JacksonProjTest {
     private String jsonChineseTemplateString = "[{\"type\":\"comment-box\",\"label_zh\":\"sdsdsd\",\"label\":\"sdsd\"},{\"type\":\"text-field\",\"label_zh\":\"ddfdf\",\"label\":\"sdsdsd\"}]";
     private String jsonEnTemplateString = "[{\"type\":\"text-field\",\"label\":\"Student Name\"},{\"type\":\"checkbox\",\"label\":\"Attended All Classes\"},{\"type\":\"checkbox\",\"label\":\"Completed All Assessments\"},{\"type\":\"comment-box\",\"label\":\"Principal Comments...\"}]";
     private String jsonEnValuesString = "[\"New Year\",null,null,\"the sun\"]";
+    private String jsonValuesWithChineseTranslations = "{\"en\":[\"New Year\",null,null,\"the sun\"],\"zh\":[\"translate\",null,null,\"translate\"]}";
+
     @Test
     public void translateOnChinese() throws Exception {
-        JacksonProj jacksonProj= new JacksonProj();
+        JacksonProj jacksonProj = new JacksonProj();
         Assert.assertTrue(jacksonProj.isTemplateContainsChineseLang(jsonChineseTemplateString));
         Assert.assertFalse(jacksonProj.isTemplateContainsChineseLang(jsonEnTemplateString));
 
         String onChinese = jacksonProj.addChineseTranslation(jsonEnValuesString);
         System.out.println(onChinese);
+        String onChinese2 = jacksonProj.addChineseTranslation(jsonValuesWithChineseTranslations);
+        System.out.println(onChinese2);
     }
 
 }
