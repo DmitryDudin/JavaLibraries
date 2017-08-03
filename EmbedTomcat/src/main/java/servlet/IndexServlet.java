69c4348c8be66e1a42975d5c971718697071c9d1
dmitrid <dudin.dmitry83@gmail.com>
2017-08-03 16:04:39 +0100

    add EmbedTomcat

---
+++ EmbedTomcat/src/main/java/servlet/IndexServlet.java
@@ -0,0 +1,27 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
+
+//@WebServlet("/gogo")
+public class IndexServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/plain");
+        response.getWriter().println("welcome!");
+    }
+
+    protected void service(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        Writer w = resp.getWriter();
+        w.write("Hello, Gog2!\n");
+        w.flush();
+    }
+
+}
+
+
